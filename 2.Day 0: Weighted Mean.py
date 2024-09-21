91ffbd4ce92f0f22ad79cdb6fbe48eb89385582a
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2024-09-22 00:23:47 +0530

    Create 2.Day 0: Weighted Mean.py

---
+++ 2.Day 0: Weighted Mean.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+    # Write your code here
+    wm=sum(a*b for a,b in zip(X,W))
+    result=round(wm/sum(W),1)
+    print(result)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
